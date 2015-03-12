e8e5e19d7e4c9cd0fa2e552c6cd6aaa8456bf2c5
Benjamin Pang <benben123213@gmail.com>
2015-03-12 23:03:22 +0000

    Updated pythians.py

--- pythians.py
+++ pythians.py
@@ -11,17 +11,20 @@ endpoint defs
 """
 @app.route('/')
 def hello_world():
-    return q[0].name
+    # return q[0].name
+    return 'Hello World!'
 
+"""
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
+"""
 
 """
 main
 """
 if __name__ == '__main__':
-    session = db.loadSession()
-    q = session.query(db.Athlete).all()
+    # session = db.loadSession()
+    # q = session.query(db.Athlete).all()
     app.run(host='0.0.0.0', port=5000)
