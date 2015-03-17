7f3c051de8a8a31e0a5f51c4d0c3aa68a64e0be8
Jacob Kovar <jacob.kovar@utexas.edu>
2015-03-17 02:06:51 +0000

    Implemented a draft of the Retrieve Country endpoint in flask. Resolves #29

--- pythians.py
+++ pythians.py
@@ -22,9 +22,32 @@ def hello(name=None):
 """
 
 """
+Scrape Country By ID
+"""
+@app.route('/scrape/countries/<int:country_id>')
+def scrape_country_by_id(country_id):
+    session = db.loadSession()
+    
+    country_info = session.query(db.Country.id,db.Country.name).filter(db.Country.id == country_id).first()
+    
+    year_info = session.query(db.Year.year).filter(db.Year.country_id == country_id).all()
+    
+    athlete_info = session.query(db.Athlete.id,db.Athlete.name).filter(db.Athlete.country_id == country_id).all()
+    
+    country_json = {'id':country_info[0],
+     'name':country_info[1],
+     'years-hosted':year_info,
+     'origin-athletes':[x for x in ({'id':x, 'name':y} for x,y in athlete_info)]}
+    
+    return str(country_json)
+
+	#str(country_info) + '<br/><br/>' + str(year_info) + '<br/><br/>' + str(athlete_info)
+
+
+"""
 main
 """
 if __name__ == '__main__':
     # session = db.loadSession()
     # q = session.query(db.Athlete).all()
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5005)
