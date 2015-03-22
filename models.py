7440212271cb16611608713654952c85bc88d359
Jacob Kovar <jacob.kovar@utexas.edu>
2015-03-22 03:25:09 +0000

    Pointed models.py to new Database

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 """
 init SQLAlchemy
 """
-engine = create_engine('postgresql://postgres:password@localhost/olympics', echo=False)
+engine = create_engine('postgresql://postgres:password@localhost/olympics_full', echo=False)
 Base = declarative_base(engine)
 
 def loadSession():
@@ -25,19 +25,23 @@ class Athlete(Base):
 class Event(Base):
     __tablename__ = 'events'
     __table_args__ = {'autoload':True}
+    
+class Sport(Base):
+    __tablename__ = 'sports'
+    __table_args__ = {'autoload':True}
+    
+class City(Base):
+    __tablename__ = 'cities'
+    __table_args__ = {'autoload':True}
 
 class Country(Base):
     __tablename__ = 'countries'
     __table_args__ = {'autoload':True}
 
-class Year(Base):
-    __tablename__ = 'years'
+class Olympics(Base):
+    __tablename__ = 'olympics'
     __table_args__ = {'autoload':True}
 
 class Medal(Base):
     __tablename__ = 'medals'
-    __table_args__ = {'autoload':True}
-    
-class Year_Representing(Base):
-    __tablename__ = 'year_representing'
     __table_args__ = {'autoload':True}
