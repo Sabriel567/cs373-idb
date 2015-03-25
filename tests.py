22d2d262d99f7959d60b3ee3fa3e06e799db7e61
Benjamin Pang <benben123213@gmail.com>
2015-03-25 22:13:37 +0000

    Added tests.py, Athletes model unit tests, closes #99

---
+++ tests.py
@@ -0,0 +1,110 @@
+
+# -------
+# imports
+# -------
+
+from unittest import main, TestCase
+import models as db
+
+# ----------
+# TestModels
+# ----------
+
+class TestModels(TestCase):
+    
+    def setUp(self):
+        self.session = db.loadSession()
+
+    def test_athletes_has_id(self):
+        self.assertTrue(hasattr(db.Athlete, "id"))
+
+    def test_athletes_has_first_name(self):
+        self.assertTrue(hasattr(db.Athlete, "first_name"))
+
+    def test_athletes_has_last_name(self):
+        self.assertTrue(hasattr(db.Athlete, "last_name"))
+
+    def test_athletes_has_gender(self):
+        self.assertTrue(hasattr(db.Athlete, "gender"))
+
+    def test_athletes_populated(self):
+        all_athletes = self.session.query(db.Athlete)\
+                                    .select_from(db.Athlete)\
+                                    .all()
+
+        self.assertTrue(len(all_athletes) > 0)
+
+    def test_athletes_id_not_null(self):
+        athlete_ids = self.session.query(db.Athlete)\
+                                    .select_from(db.Athlete)\
+                                    .filter(db.Athlete.id == None)\
+                                    .all()
+        
+        self.assertTrue(len(athlete_ids) == 0)
+
+    def test_athletes_first_name_not_null(self):
+        athlete_first_names = self.session.query(db.Athlete)\
+                                    .select_from(db.Athlete)\
+                                    .filter(db.Athlete.first_name == None)\
+                                    .all()
+
+        self.assertTrue(len(athlete_first_names) == 0)
+
+    def test_athletes_last_name_not_null(self):
+        athlete_last_names = self.session.query(db.Athlete)\
+                                    .select_from(db.Athlete)\
+                                    .filter(db.Athlete.last_name == None)\
+                                    .all()
+
+        self.assertTrue(len(athlete_last_names) == 0)
+
+    def test_athletes_gender_not_null(self):
+        athlete_genders = self.session.query(db.Athlete)\
+                                    .select_from(db.Athlete)\
+                                    .filter(db.Athlete.gender == None)\
+                                    .all()
+
+        self.assertTrue(len(athlete_genders) == 0)
+
+    def test_athletes_id_1(self):
+        athlete = self.session.query(db.Athlete.first_name,
+                                            db.Athlete.last_name,
+                                            db.Athlete.gender)\
+                                    .select_from(db.Athlete)\
+                                    .filter(db.Athlete.id == 13839)\
+                                    .all()[0]
+        
+        self.assertEqual(athlete[0], "Monique")
+        self.assertEqual(athlete[1], "Knol")
+        self.assertEqual(athlete[2], "Women")
+
+    def test_athletes_id_2(self):
+        athlete = self.session.query(db.Athlete.first_name,
+                                            db.Athlete.last_name,
+                                            db.Athlete.gender)\
+                                    .select_from(db.Athlete)\
+                                    .filter(db.Athlete.id == 9493)\
+                                    .all()[0]
+
+        self.assertEqual(athlete[0], "Emma")
+        self.assertEqual(athlete[1], "Gapchenko")
+        self.assertEqual(athlete[2], "Women")
+
+    def test_athletes_id_3(self):
+        athlete = self.session.query(db.Athlete.first_name,
+                                            db.Athlete.last_name,
+                                            db.Athlete.gender)\
+                                    .select_from(db.Athlete)\
+                                    .filter(db.Athlete.id == 9492)\
+                                    .all()[0]
+
+        self.assertEqual(athlete[0], "Doreen Viola Hansen")
+        self.assertEqual(athlete[1], "Wilber")
+        self.assertEqual(athlete[2], "Women")
+
+# ----
+# main
+# ----
+
+if __name__ == "__main__":
+    main()
