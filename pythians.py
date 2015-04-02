aad95b2d10e42017c15658a38f90d9e465b062a1
Benjamin Pang <benben123213@gmail.com>
2015-04-02 05:50:15 +0000

    Fixed incorrect all_countries query in /countries endpoint

--- pythians.py
+++ pythians.py
@@ -880,9 +880,9 @@ def countries():
                                 func.count(db.Medal.id), 
                                 func.count(distinct(db.Medal.athlete_id)))\
                                 .select_from(db.Country)\
-                                .join(db.City)\
-                                .join(db.Olympics)\
-                                .join(db.Medal)\
+                                .outerjoin(db.City)\
+                                .outerjoin(db.Olympics)\
+                                .outerjoin(db.Medal)\
                                 .group_by(db.Country.name, db.Country.id)\
                                 .all()
 
@@ -934,6 +934,8 @@ def country_id(country_id):
     # total medals overall
     total_medals = total_gold_count_medals[0][1]
     total_gold_medals = total_gold_count_medals[0][0]
+
+    print(total_medals)
 
     # total athletes
     total_athletes = session.query(func.count(distinct(db.Medal.athlete_id)))\
