2b568a7c8974027c5df59d1c056007786a556767
Hannah <hannahb@cs.utexas.edu>
2015-04-05 03:55:04 +0000

    Fixes #138 Cleaned up the query for the games id endpoint

--- db/shrinkData.py
+++ db/shrinkData.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import sessionmaker
 """
 init SQLAlchemy
 """
-engine = create_engine('postgresql://postgres:password@localhost/olympics', echo=False, isolation_level="AUTOCOMMIT")
+engine = create_engine('postgresql://postgres@localhost/olympics_dev', echo=False, isolation_level="AUTOCOMMIT")
 Base = declarative_base(engine)
 conn = engine.connect()
 
@@ -23,19 +23,16 @@ join olympics as o on o.id = m.olympic_id
 where 
         e.name  not like '%team%' 
         and e.name  not like '%doubles%'
-        and e.name not in ('basketball', 'baseball', 'football', 'handball', 'hockey')
+        and e.name  not like '%relay%'
+        and e.name not ilike '%synchronized%' 
+        and e.name not ilike '%four%'
+        and e.name not ilike '%%'
+        and e.name not in ('basketball', 'baseball', 'football', 'handball', 'hockey', 'team competition', 'volleyball', 'water polo')
         and o.year not in (1968,1972,1976,1980,1984,1988,1992,1996,2000,2004)
         and s.id not in (1,2,3, 10,11,15,4,17,16,20,25,28,32,34)
 order by year, s.id, e.id
-"""
 
-
-def create_sub_sql(created_table_id, pull_table):
-  return """select {1}.{0}id from medals as m
-          join events as e on e.id = m.event_id
-          join olympics as o on o.id = m.olympic_id
-          join cities as c on c.id = o.city_id
-          where m.id in (3,4,7,
+where m.id in (3,4,7,
                           1212,1213,1215,
                           2446,2498,2515,
                           3950,3927,3936,3920,
@@ -47,127 +44,160 @@ def create_sub_sql(created_table_id, pull_table):
                           14909,14907,14925,14927,
                           17179,17180,17162
                           )
-          group by {1}.{0}id""".format(created_table_id, pull_table)
-"""
-  SPORTS
-"""
-created_table_id = "sport_"
-created_table = "sports"
-pull_table = "e"
-table_columns = "id, name"
-table_column_types = "id int, name text"
-sql = """INSERT INTO {2} select {1}  from 
-      dblink('dbname=olympics_full'::text, 
-        'select {1} from {2} 
-        where id in ({0})') AS t1 ({3})
-      """.format(create_sub_sql(created_table_id, pull_table), table_columns, created_table, table_column_types)
-print(sql)
-conn.execute(sql)
-
 
 """
-  EVENTS
-"""
-created_table_id = "event_"
-created_table = "events"
-pull_table = "m"
-table_columns = "id, sport_id, name, gender"
-table_column_types = "id int, sport_id int, name text, gender text"
-sql = """INSERT INTO {2} select {1}  from 
-      dblink('dbname=olympics_full'::text, 
-        'select {1} from {2} 
-        where id in ({0})') AS t1 ({3})
-      """.format(create_sub_sql(created_table_id, pull_table), table_columns, created_table, table_column_types)
-print(sql)
-conn.execute(sql)
-  
-"""
-  COUNTRIES
-"""
-created_table_id = "country_"
-created_table = "countries"
-pull_table = "m"
-table_columns = "id, name, noc"
-table_column_types = "id int, name text, noc text"
-sql = """INSERT INTO {2} select {1}  from 
-      dblink('dbname=olympics_full'::text, 
-        'select {1} from {2} 
-        where id in ({0})') AS t1 ({3})
-      """.format(create_sub_sql(created_table_id, pull_table), table_columns, created_table, table_column_types)
-print(sql)
-conn.execute(sql)
+medal_ids ="""select m.id from medals as m
+        join events as e on m.event_id = e.id
+        where e.name  not ilike ''%%team%%'' 
+        and e.name  not ilike ''%%double%%'' 
+        and e.name not ilike ''%%group%%'' 
+        and e.name not ilike ''%%duet%%''
+        and e.name  not ilike ''%%relay%%''
+        and e.name not ilike ''%%synchronized%%'' 
+        and e.name not ilike ''%%four%%'' 
+        and e.name not ilike ''%%eight%%'' 
+        and e.name not ilike ''%%quadruple%%''
+        and e.name not ilike ''%%two%%''
+        and e.name not ilike ''%%tandem%%''
+        and e.name not ilike ''%%pair%%''
+        and e.name not in (''basketball'', ''baseball'', ''football'', ''handball'', ''hockey'', ''softball'', ''team competition'', ''volleyball'', ''water polo'', ''flying dutchman'')"""
 
-pull_table = "c"
-sql = """INSERT INTO {2} select {1}  from 
-      dblink('dbname=olympics_full'::text, 
-        'select {1} from {2} 
-        where id in ({0}) and id not in ({4})') AS t1 ({3})
-      """.format(create_sub_sql(created_table_id, pull_table), table_columns, created_table, table_column_types,create_sub_sql(created_table_id, "m"))
-print(sql)
-conn.execute(sql)
+def create_sub_sql(created_table_id, pull_table):
+  return """select {1}.{0}id from medals as m
+          join events as e on e.id = m.event_id
+          join olympics as o on o.id = m.olympic_id
+          join cities as c on c.id = o.city_id
+          where m.id in ({2}
+                          )
+          group by {1}.{0}id""".format(created_table_id, pull_table, medal_ids)
+def main():
+  """
+    SPORTS
+  """
+  created_table_id = "sport_"
+  created_table = "sports"
+  pull_table = "e"
+  table_columns = "id, name"
+  table_column_types = "id int, name text"
+  sql = """INSERT INTO {2} select {1}  from 
+        dblink('dbname=olympics_full'::text, 
+          'select {1} from {2} 
+          where id in ({0})') AS t1 ({3})
+        """.format(create_sub_sql(created_table_id, pull_table), table_columns, created_table, table_column_types)
+  print(type(sql))
+  print(sql)
+  conn.execute(sql)
+
+
+  """
+    EVENTS
+  """
+  created_table_id = "event_"
+  created_table = "events"
+  pull_table = "m"
+  table_columns = "id, sport_id, name, gender"
+  table_column_types = "id int, sport_id int, name text, gender text"
+  sql = """INSERT INTO {2} select {1}  from 
+        dblink('dbname=olympics_full'::text, 
+          'select {1} from {2} 
+          where id in ({0})') AS t1 ({3})
+        """.format(create_sub_sql(created_table_id, pull_table), table_columns, created_table, table_column_types)
+  print(sql)
+  conn.execute(sql)
+    
+  """
+    COUNTRIES
+  """
+  created_table_id = "country_"
+  created_table = "countries"
+  pull_table = "m"
+  table_columns = "id, name, noc"
+  table_column_types = "id int, name text, noc text"
+  sql = """INSERT INTO {2} select {1}  from 
+        dblink('dbname=olympics_full'::text, 
+          'select {1} from {2} 
+          where id in ({0})') AS t1 ({3})
+        """.format(create_sub_sql(created_table_id, pull_table), table_columns, created_table, table_column_types)
+  print(sql)
+  conn.execute(sql)
+
+  pull_table = "c"
+  sql = """INSERT INTO {2} select {1}  from 
+        dblink('dbname=olympics_full'::text, 
+          'select {1} from {2} 
+          where id in ({0}) and id not in ({4})') AS t1 ({3})
+        """.format(create_sub_sql(created_table_id, pull_table), table_columns, created_table, table_column_types,create_sub_sql(created_table_id, "m"))
+  print(sql)
+  conn.execute(sql)
+
+  """
+    CITIES
+  """
+  created_table_id = "city_"
+  created_table = "cities"
+  pull_table = "o"
+  table_columns = "id, name, country_id"
+  table_column_types = "id int, name text, country_id int"
+  sql = """INSERT INTO {2} select {1}  from 
+        dblink('dbname=olympics_full'::text, 
+          'select {1} from {2} 
+          where id in ({0})') AS t1 ({3})
+        """.format(create_sub_sql(created_table_id, pull_table), table_columns, created_table, table_column_types)
+  print(sql)
+  conn.execute(sql)
+
+  """
+    Athletes
+  """
+  created_table_id = "athlete_"
+  created_table = "athletes"
+  pull_table = "m"
+  table_columns = "id, first_name, last_name, gender"
+  table_column_types = "id int, first_name text, last_name text, gender text"
+  sql = """INSERT INTO {2} select {1}  from 
+        dblink('dbname=olympics_full'::text, 
+          'select {1} from {2} 
+          where id in ({0})') AS t1 ({3})
+        """.format(create_sub_sql(created_table_id, pull_table), table_columns, created_table, table_column_types)
+  print(sql)
+  conn.execute(sql)
+
+  """
+    Olympics
+  """
+  created_table_id = "olympic_"
+  created_table = "olympics"
+  pull_table = "m"
+  table_columns = "id, year, season, city_id"
+  table_column_types = "id int, year int, season text, city_id int"
+  sql = """INSERT INTO {2} select {1}  from 
+        dblink('dbname=olympics_full'::text, 
+          'select {1} from {2} 
+          where id in ({0})') AS t1 ({3})
+        """.format(create_sub_sql(created_table_id, pull_table), table_columns, created_table, table_column_types)
+  created_table_id = "sport"
+  print(sql)
+  conn.execute(sql)
+
+  """
+    Medals 
+  """
+  created_table_id = ""
+  created_table = "medals"
+  pull_table = "m"
+  table_columns = "id, rank, athlete_id, event_id, country_id, olympic_id"
+  table_column_types = "id int, rank text, athlete_id int, event_id int, country_id int, olympic_id int"
+  sql = """INSERT INTO {2} select {1}  from 
+        dblink('dbname=olympics_full'::text, 
+          'select {1} from {2} 
+          where id in ({0})') AS t1 ({3})
+        """.format(create_sub_sql(created_table_id, pull_table), table_columns, created_table, table_column_types)
+  created_table_id = "sport"
+  print(sql)
+  conn.execute(sql)
 
 """
-  CITIES
+main
 """
-created_table_id = "city_"
-created_table = "cities"
-pull_table = "o"
-table_columns = "id, name, country_id"
-table_column_types = "id int, name text, country_id int"
-sql = """INSERT INTO {2} select {1}  from 
-      dblink('dbname=olympics_full'::text, 
-        'select {1} from {2} 
-        where id in ({0})') AS t1 ({3})
-      """.format(create_sub_sql(created_table_id, pull_table), table_columns, created_table, table_column_types)
-print(sql)
-conn.execute(sql)
-
-"""
-  Athletes
-"""
-created_table_id = "athlete_"
-created_table = "athletes"
-pull_table = "m"
-table_columns = "id, first_name, last_name, gender"
-table_column_types = "id int, first_name text, last_name text, gender text"
-sql = """INSERT INTO {2} select {1}  from 
-      dblink('dbname=olympics_full'::text, 
-        'select {1} from {2} 
-        where id in ({0})') AS t1 ({3})
-      """.format(create_sub_sql(created_table_id, pull_table), table_columns, created_table, table_column_types)
-print(sql)
-conn.execute(sql)
-
-"""
-  Olympics
-"""
-created_table_id = "olympic_"
-created_table = "olympics"
-pull_table = "m"
-table_columns = "id, year, season, city_id"
-table_column_types = "id int, year int, season text, city_id int"
-sql = """INSERT INTO {2} select {1}  from 
-      dblink('dbname=olympics_full'::text, 
-        'select {1} from {2} 
-        where id in ({0})') AS t1 ({3})
-      """.format(create_sub_sql(created_table_id, pull_table), table_columns, created_table, table_column_types)
-created_table_id = "sport"
-print(sql)
-conn.execute(sql)
-
-"""
-  Medals 
-"""
-created_table_id = ""
-created_table = "medals"
-pull_table = "m"
-table_columns = "id, rank, athlete_id, event_id, country_id, olympic_id"
-table_column_types = "id int, rank text, athlete_id int, event_id int, country_id int, olympic_id int"
-sql = """INSERT INTO {2} select {1}  from 
-      dblink('dbname=olympics_full'::text, 
-        'select {1} from {2} 
-        where id in ({0})') AS t1 ({3})
-      """.format(create_sub_sql(created_table_id, pull_table), table_columns, created_table, table_column_types)
-created_table_id = "sport"
-print(sql)
-conn.execute(sql)
+if __name__ == '__main__':
+    main();
--- pythians.py
+++ pythians.py
@@ -246,117 +246,39 @@ def games_id(game_id):
     host_city = host_query[0][0]
     year = host_query[0][1]
 
-    athletes_query = session.query(distinct(db.Athlete.id).label('athlete_id'))\
-                            .select_from(db.Athlete)\
-                            .join(db.Medal)\
-                            .filter(game_id == db.Medal.olympic_id)\
-                            .subquery()
+    top_athletes_query = session.query(db.Athlete.id, db.Athlete.first_name, db.Athlete.last_name,
+                                        db.Country.id, db.Country.name,
+                                        func.sum(case([(db.Medal.rank=='Gold', 1)], else_=0)).label('gold'), 
+                                        func.sum(case([(db.Medal.rank=='Silver', 1)], else_=0)).label('silver'), 
+                                        func.sum(case([(db.Medal.rank=='Bronze', 1)], else_=0)).label('bronze')
+                                      )\
+                                      .select_from(db.Athlete)\
+                                      .join(db.Medal)\
+                                      .join(db.Country)\
+                                      .filter(db.Medal.olympic_id == game_id)\
+                                      .group_by(db.Athlete.id, db.Athlete.first_name, db.Athlete.last_name,
+                                                db.Country.id, db.Country.name)\
+                                      .order_by(func.sum(case([(db.Medal.rank=='Gold', 1)], else_=0)))\
+                                      .limit(3)\
+                                      .all()
 
-    total_athletes_g = session.query(athletes_query.c.athlete_id.label('id'), 
-                                    func.count(db.Medal.rank).label('num_gold'))\
-                            .select_from(db.Medal)\
-                            .filter(db.Medal.rank == "Gold")\
-                            .filter(game_id == db.Medal.olympic_id)\
-                            .join(athletes_query, db.Medal.athlete_id == athletes_query.c.athlete_id)\
-                            .group_by(athletes_query.c.athlete_id)\
-                            .subquery()
-
-    total_athletes_s = session.query(athletes_query.c.athlete_id.label('id'), 
-                                    func.count(db.Medal.rank).label('num_silver'))\
-                            .select_from(db.Medal)\
-                            .filter(db.Medal.rank == "Silver")\
-                            .filter(game_id == db.Medal.olympic_id)\
-                            .join(athletes_query, db.Medal.athlete_id == athletes_query.c.athlete_id)\
-                            .group_by(athletes_query.c.athlete_id)\
-                            .subquery()
-
-    total_athletes_b = session.query(athletes_query.c.athlete_id.label('id'), 
-                                    func.count(db.Medal.rank).label('num_bronze'))\
-                            .select_from(db.Medal)\
-                            .filter(db.Medal.rank == "Bronze")\
-                            .filter(game_id == db.Medal.olympic_id)\
-                            .join(athletes_query, db.Medal.athlete_id == athletes_query.c.athlete_id)\
-                            .group_by(athletes_query.c.athlete_id)\
-                            .subquery()
-
-    top_athletes_query = session.query(distinct(db.Athlete.id), 
-                                        db.Athlete.first_name, 
-                                        db.Athlete.last_name,
-                                        db.Country.id,
-                                        db.Country.name,
-                                        coalesce(total_athletes_g.c.num_gold, 0),
-                                        coalesce(total_athletes_s.c.num_silver, 0),
-                                        coalesce(total_athletes_b.c.num_bronze, 0))\
-                                .select_from(db.Athlete)\
-                                .join(db.Medal)\
-                                .filter(game_id == db.Medal.olympic_id)\
-                                .join(db.Country)\
-                                .outerjoin(total_athletes_g, and_(db.Athlete.id == total_athletes_g.c.id))\
-                                .outerjoin(total_athletes_s, and_(db.Athlete.id == total_athletes_s.c.id))\
-                                .outerjoin(total_athletes_b, and_(db.Athlete.id == total_athletes_b.c.id))\
-                                .order_by(coalesce(total_athletes_g.c.num_gold, 0).desc())\
-                                .limit(4)\
-                                .all()
 
     for r in top_athletes_query:
         top_athletes.append({'athlete_id':r[0], 'athlete_name':r[1] + " " + r[2], 'country_id':r[3], 'country_name':r[4], 'num_gold':r[5], 'num_silver':r[6], 'num_bronze':r[7]})
 
-    
-    countries_query = session.query(distinct(db.Country.id).label('country_id'))\
-                            .select_from(db.Country)\
-                            .join(db.Medal)\
-                            .filter(game_id == db.Medal.olympic_id)\
-                            .subquery()
 
-    medals_query = session.query(db.Medal.event_id.label('event_id'), 
-                                    db.Medal.country_id.label('country_id'), 
-                                    db.Medal.rank.label('rank'), 
-                                    db.Medal.olympic_id.label('olympic_id'))\
-                            .select_from(db.Medal)\
-                            .group_by(db.Medal.event_id, db.Medal.rank, db.Medal.country_id, db.Medal.olympic_id)\
-                            .subquery()
-
-    total_countries_g = session.query(countries_query.c.country_id.label('id'),
-                                        func.count(medals_query.c.rank).label('num_gold'))\
-                            .select_from(medals_query)\
-                            .filter(medals_query.c.rank == "Gold")\
-                            .filter(game_id == medals_query.c.olympic_id)\
-                            .join(countries_query, medals_query.c.country_id == countries_query.c.country_id)\
-                            .group_by(countries_query.c.country_id)\
-                            .subquery()
-
-    total_countries_s = session.query(countries_query.c.country_id.label('id'),
-                                        func.count(medals_query.c.rank).label('num_silver'))\
-                            .select_from(medals_query)\
-                            .filter(medals_query.c.rank == "Silver")\
-                            .filter(game_id == medals_query.c.olympic_id)\
-                            .join(countries_query, medals_query.c.country_id == countries_query.c.country_id)\
-                            .group_by(countries_query.c.country_id)\
-                            .subquery()
-
-    total_countries_b = session.query(countries_query.c.country_id.label('id'),
-                                        func.count(medals_query.c.rank).label('num_bronze'))\
-                            .select_from(medals_query)\
-                            .filter(medals_query.c.rank == "Bronze")\
-                            .filter(game_id == medals_query.c.olympic_id)\
-                            .join(countries_query, medals_query.c.country_id == countries_query.c.country_id)\
-                            .group_by(countries_query.c.country_id)\
-                            .subquery()
-
-    top_countries_query = session.query(distinct(db.Country.id), 
-                                        db.Country.name,
-                                        coalesce(total_countries_g.c.num_gold, 0),
-                                        coalesce(total_countries_s.c.num_silver, 0),
-                                        coalesce(total_countries_b.c.num_bronze, 0))\
-                                .select_from(db.Country)\
-                                .join(db.Medal)\
-                                .filter(game_id == db.Medal.olympic_id)\
-                                .outerjoin(total_countries_g, and_(db.Country.id == total_countries_g.c.id))\
-                                .outerjoin(total_countries_s, and_(db.Country.id == total_countries_s.c.id))\
-                                .outerjoin(total_countries_b, and_(db.Country.id == total_countries_b.c.id))\
-                                .order_by(coalesce(total_countries_g.c.num_gold, 0).desc())\
-                                .limit(4)\
-                                .all()
+    top_countries_query = session.query(db.Country.id, db.Country.name,
+                                        func.sum(case([(db.Medal.rank=='Gold', 1)], else_=0)).label('gold'), 
+                                        func.sum(case([(db.Medal.rank=='Silver', 1)], else_=0)).label('silver'), 
+                                        func.sum(case([(db.Medal.rank=='Bronze', 1)], else_=0)).label('bronze')
+                                      )\
+                                      .select_from(db.Athlete)\
+                                      .join(db.Medal)\
+                                      .join(db.Country)\
+                                      .filter(db.Medal.olympic_id == game_id)\
+                                      .group_by(db.Country.id, db.Country.name)\
+                                      .order_by(func.sum(case([(db.Medal.rank=='Gold', 1)], else_=0)).desc())\
+                                      .all()
     
     for r in top_countries_query:
         top_countries.append({'country_id':r[0], 'country_name':r[1], 'num_gold':r[2], 'num_silver':r[3], 'num_bronze':r[4]})
