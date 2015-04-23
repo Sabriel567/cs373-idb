3f5687587158aaf5e58169d649090a3d2b9298f5
Hannah <hannahb@cs.utexas.edu>
2015-04-23 23:25:58 +0000

    Fixes #155 Fixed scraper to download athlete photos

--- db/getPhotos.py
+++ db/getPhotos.py
@@ -19,26 +19,45 @@ conn = engine.connect()
 
 rows = conn.execute("Select first_name::text || ' ' || last_name::text from athletes")
 
+save_base = "/home/hannahb/cs373-idb/static/img/scrapeathlete/" 
+base_url = "http://www.olympic.org"
+
+url = "http://www.olympic.org/"
+
+"""
+page = urlopen(url)
+soup = BeautifulSoup(page.read())
+
+path = soup.findAll(id='ctl00_mainContent_AthleteIdentityCardBlock_CardDetails_IdentityCardImage_HtmlImage1')
+if(path != []):
+  imgUrl = path[0]["src"]
+  print("url: {0}".format(base_url + imgUrl))
+  urllib.request.urlretrieve((base_url + imgUrl).replace(' ', '%20'), save_base + "Michael_Phelps.jpg")
+"""
+
 num_found = 0;
 not_found = 0;
+checked = 0;
 for r in rows:
-  athlete_name = r[0]
-  print(athlete_name)
+  if (checked % 100 == 0):
+    print("Checked: {0}; Found: {1}".format(checked, num_found))
+  checked = checked + 1 
 
+  athlete_name = r[0]
   url = "http://www.olympic.org/{0}".format(athlete_name.replace(' ','-'))
   try:
     page = urlopen(url)
     soup = BeautifulSoup(page.read())
 
-    path = soup.find(id='ctl00_mainContent_AthleteIdentityCardBlock_CardDetails_IdentityCardImage_HtmlImage1')
-    if(path != None):
+    path = soup.findAll(id='ctl00_mainContent_AthleteIdentityCardBlock_CardDetails_IdentityCardImage_HtmlImage1')
+    if(path != []):
       num_found = num_found + 1;
-      print("Found: {0}".format(num_found))
+      imgUrl = path[0]["src"]
+      save_path = (save_base + athlete_name.replace(' ','_') + ".jpg")
+      urllib.request.urlretrieve((base_url + imgUrl).replace(' ', '%20'), save_path)
     else: 
-      print("Not Found: {0}".format(not_found))
       not_found = not_found + 1;
   except: 
-    print('EXCEPTION')
     pass
 
 
